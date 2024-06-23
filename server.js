0be21d5a1a9ec7a08db5d96b104de0651effff42
Shobhit <vipershob01@gmail.com>
2024-06-23 17:07:13 +0530

    backend blackcoffer

--- server.js
+++ server.js
@@ -1,41 +1,60 @@
 const express = require('express');
 const app = express();
-
-
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
 dotenv.config();
-const morgan = require('morgan')
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://bc-frontend-shobhit.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 
+const allowedOrigins = ["https://bc-frontend-shobhit.vercel.app"];
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true 
+}));
 
-app.use(cors({origin : "https://bc-frontend-shobhit.vercel.app" , ceredentials : "true"}))
 app.use(express.json());
 
-
-//database
-mongoose.connect(process.env.MONGO_URL)
-mongoose.connection.on('connected', () => {
-    console.log('connected to database')
+// Database connection
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-mongoose.connection.on('error', () => {
-    console.log('connection failed')
+.then(() => {
+    console.log('Connected to MongoDB');
 })
-
+.catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+});
 
 // Routes
 const dataRoutes = require('./routes/dataRoutes');
 app.use('/api', dataRoutes);
-const authRoutes = require('./routes/userRoutes')
-app.use('/auth', authRoutes)
 
-app.get('/' , async(req,res) => {
-    res.send('working')
-})  
+const authRoutes = require('./routes/userRoutes');
+app.use('/auth', authRoutes);
 
-//server
+app.get('/', async (req, res) => {
+    res.send('working');
+});
+
+// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
